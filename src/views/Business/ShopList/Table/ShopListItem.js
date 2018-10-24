4410546ff908337a6d4281ea75295cb7b908cc0d
Micalgenus <micalgenus@gmail.com>
2018-10-24 17:03:51 +0900

    Modify button event area.

--- src/views/Business/ShopList/Table/ShopListItem.js
+++ src/views/Business/ShopList/Table/ShopListItem.js
@@ -9,8 +9,10 @@ export default class ShopListItem extends Component {
         <td>{this.props.PHONE}</td>
         <td>{this.props.HOMEPAGE}</td>
         <td className="text-center">0</td>
-        <td className="text-center" onClick={() => this.props.selectShop(this.props._id)}>
-          <button disabled={this.props.selected}>선택</button>
+        <td className="text-center">
+          <button onClick={() => this.props.selectShop(this.props._id)} disabled={this.props.selected}>
+            선택
+          </button>
         </td>
       </tr>
     );
